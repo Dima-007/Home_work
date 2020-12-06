361c0cae7685830ecd1aaadade737235f027820c
dima-007 <medinskiy18@gmail.com>
2020-12-06 15:47:45 +0300

    Home work №5

---
+++ leson-5.py
@@ -0,0 +1,114 @@
+#values ​​= 56534800
+#my_symbol = "0"
+
+#count = values.count (my_symbol)
+#print (количество)
+
+########################################
+
+#value = 34544390908560000
+#count = 0
+# пока значение% 10 == 0:
+# count + = 1
+# значение // = 10
+#print (количество)
+
+########################################
+
+# my_list_1 = [1, 2, 3, 4, 5]
+# my_list_2 = [10, 15, 20, 25]
+#my_result = []
+
+# для значения в my_list_1:
+# если значение% 2 == 0:
+# my_result.append (значение)
+
+# для значения в my_list_2:
+# если значение% 2 == 1:
+# my_result.append (значение)
+
+#print (my_result)
+
+########################################
+
+#my_list = [1, 2, 3, 4, 5]
+#new_list = []
+
+# для символа в my_list:
+# если символа нет в new_list:
+# new_list.append (символ)
+
+# new_list.append (новый_лист [0])
+# new_list.pop (0)
+#print (новый_лист)
+
+########################################
+
+#my_list = [1, 2, 3, 4, 5]
+
+# my_list.append (my_list [0])
+# my_list.pop (0)
+#print (my_list)
+
+
+########################################
+
+#my_str = '43 больше чем 34 но меньше чем 56 '
+#new_str = my_str.split ()
+#my_list = []
+
+
+# для индекса в new_str:
+# если index.isdigit () == True:
+# my_list.append (int (индекс))
+
+#print (сумма (my_list))
+
+########################################
+#import re
+#my_str = 'qwerrrtyu'
+# my_str_2 = '_'
+
+#if len (my_str)% 2 == 1:
+# my_str = my_str + my_str_2
+
+#items = re.findall (r '. {1,2}', my_str)
+
+#print (элементы)
+
+########################################
+
+#my_str = "Моя_длинная строка"
+#l_limit = "o"
+#r_limit = "t"
+#sub_str = []
+
+#index_l = my_str.find (l_limit) +1
+#index_r = my_str.find (r_limit)
+#sub_str = my_str [index_l: index_r]
+
+#print (sub_str)
+
+########################################
+
+#my_str = "Моя длинная строка"
+#l_limit = "o"
+#r_limit = "g"
+#sub_str = []
+
+#index_l = my_str.find (l_limit) +1
+#index_r = my_str.rfind (r_limit)
+#sub_str = my_str [index_l: index_r]
+
+
+#print (sub_str)
+
+
+########################################
+
+#my_list = [2,4,1,5,3,9,0,7]
+#counter = 0
+# для индекса в диапазоне (1, len (my_list) - 1):
+# if my_list [index - 1] <my_list [index]> my_list [index + 1]:
+# counter + = 1
+#print (счетчик)
